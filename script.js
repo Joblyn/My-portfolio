e8c1a84bad0764daf70a53e8444f28bb5c22986d
Joblyn <oaikhenahjob@gmail.com>
2021-02-13 18:18:51 +0100

    scroll fix

--- script.js
+++ script.js
@@ -1,22 +1,5 @@
 window.scroll({
- top: 2500,
-  left: 0,
-  behaviour: 'smooth'  
+    // top: 2500,
+    // left: 0,
+    behaviour: 'smooth'
 });
-
-$(function() {
-  $('a[href*=#]:not([href=#])').click(function() {
-    if (location.pathname.replace(/^\//,'') == this.pathname.replace(/^\//,'') && location.hostname == this.hostname) {
-      var target = $(this.hash);
-      target = target.length ? target : $('[name=' + this.hash.slice(1) +']');
-      if (target.length) {
-        $('html,body').animate({
-          scrollTop: target.offset().top
-        }, 1000);
-        return false;
-      }
-    }
-  });
-});
-
-
