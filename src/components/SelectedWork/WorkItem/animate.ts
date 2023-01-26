16aa3d126a4fe0cb015960548013d5f905a05b9f
Job Oaikhenah <62160764+Joblyn@users.noreply.github.com>
2023-01-26 23:50:07 +0100

    Update on work item animation

--- src/components/SelectedWork/WorkItem/animate.ts
+++ src/components/SelectedWork/WorkItem/animate.ts
@@ -8,13 +8,14 @@ export const enableDrag = () => {
     .querySelector(".work__image img")
     ?.getBoundingClientRect() as DOMRect;
   const date = new Date();
-  const friction = 0.9;
+  const friction = 9.8;
   let initRect: DOMRect,
-    currRect: DOMRect,
+    endRect: DOMRect,
     velocity: number,
     startTime: number,
     endTime: number,
-    dist: number;
+    dist: number,
+    direction: "left" | "right";
 
   Draggable.create(".work__image img", {
     type: "x",
@@ -25,26 +26,37 @@ export const enableDrag = () => {
       startTime = date.getTime();
       initRect = this.target.getBoundingClientRect();
     },
+    onDrag: function () {
+      // update direction every 500ms on move
+      const { left: oldLeft } = this.target.getBoundingClientRect();
+      setInterval(() => {
+        const { left: newLeft } = this.target.getBoundingClientRect();
+        direction = newLeft < oldLeft ? "left" : "right";
+      }, 500);
+
+      // direction = currRect.left < initRect.left => left
+      //             currRect.left > initRect.left => right
+    },
     onDragEnd: function () {
       endTime = date.getTime();
-      currRect = this.target.getBoundingClientRect();
-      dist = Math.abs(Math.abs(initRect.left) - Math.abs(currRect.left));
+      endRect = this.target.getBoundingClientRect();
+      dist = Math.abs(Math.abs(endRect.left) - Math.abs(initRect.left));
       velocity = dist / ((endTime - startTime) / 60);
 
       // if left is greater than initial left, snap back to position;
-      const exceededPosLeft = targetRect.left < currRect.left ? true : false;
+      const exceededPosLeft = targetRect.left < endRect.left ? true : false;
       if (exceededPosLeft) snapToPosition(this.target, 0);
 
+      const initRight = endRect.width - targetRect.left;
       const totalDist =
-        Math.abs(Math.abs(currRect.left) - Math.abs(targetRect.left)) +
+        Math.abs(Math.abs(endRect.left) - Math.abs(targetRect.left)) +
         window.innerWidth;
-      const initRight = currRect.width - targetRect.left;
-      const exceededPosRight =
-        totalDist > initRight ? currRect.width - window.innerWidth : false;
       // if right is less than initial right, snap back to position;
+      const exceededPosRight =
+        totalDist > initRight ? endRect.width - window.innerWidth : false;
       if (exceededPosRight) snapToPosition(this.target, `-${exceededPosRight}`);
 
-      // slowDown(this.target);
+      slowDown(this.target);
     },
   });
 
@@ -52,12 +64,18 @@ export const enableDrag = () => {
     gsap.to(target, {
       x,
       duration: 0.3,
-      ease: "power1.out",
+      ease: "Power2.easOut",
     });
   };
 
   const slowDown = (target: HTMLElement) => {
-    const vx = dist * friction;
+    // distToStop => dist; speed (d/t); friction;
+    // velocity => direction of speed
+    // currRect.left(-v)
+    // direction = left => snap to less x => dist
+    // (Math.abs(currRect.left + targetRect.left + snapDist)(-ve)
+    // direction = right => snap to greater x => currRect.left + targetRect.left + snapDist
+    const vx = velocity / (2 * friction);
     const vt = (dist / velocity) * ((endTime - startTime) / 60);
 
     gsap.to(target, {
@@ -67,6 +85,6 @@ export const enableDrag = () => {
     });
 
     // s = ut - (1/2)gt^2
-    // v = u^2 - 2gs => s = (u^2 - v)/2g
+    // v^2 = u^2 - 2gs => s = (u^2 - v^2)/2g
   };
 };
