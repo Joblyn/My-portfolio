b6659d61448d0113278ebb1f6a21926d273f353c
Job Oaikhenah <62160764+Joblyn@users.noreply.github.com>
2023-01-30 00:49:07 +0100

    Update on work item drag animation

--- src/components/SelectedWork/WorkItem/animate.ts
+++ src/components/SelectedWork/WorkItem/animate.ts
@@ -8,8 +8,8 @@ export const enableDrag = () => {
     .querySelector(".work__image img")
     ?.getBoundingClientRect() as DOMRect;
 
-  console.log("targetRect.left", targetRect.left);
-  const friction = 9.8;
+  const friction = 5;
+  const duration = 0.6;
   let initRect: DOMRect,
     endRect: DOMRect,
     velocity: number,
@@ -33,26 +33,18 @@ export const enableDrag = () => {
       const { left: newLeft } = this.target.getBoundingClientRect();
       if (oldLeft) {
         direction = newLeft < oldLeft ? "left" : "right";
-        console.log("direction", direction);
       }
       oldLeft = newLeft;
     },
     onDragEnd: function () {
-      console.log("drag end");
       endTime = new Date().getTime();
       endRect = this.target.getBoundingClientRect();
       dist = Math.abs(Math.abs(endRect.left) - Math.abs(initRect.left));
-      console.log("distance travelled", `${dist} px`);
-      console.log("startTime", startTime);
-      console.log("endTime", endTime);
       tt = (endTime - startTime) / 1000;
-      console.log("time taken", `${tt} + "s"`);
       velocity = dist / tt;
-      console.log("velocity", `${velocity} px/s`);
 
       // if left is greater than initial left, snap back to position;
       const exceededPosLeft = targetRect.left < endRect.left ? true : false;
-      console.log("exceededPosLeft", exceededPosLeft);
       if (exceededPosLeft) {
         return snapToPosition(this.target, 0);
       }
@@ -64,9 +56,6 @@ export const enableDrag = () => {
       // if right is less than initial right, snap back to position;
       const exceededPosRight =
         totalDist > initRight ? endRect.width - window.innerWidth : false;
-      console.log("totalDist", totalDist);
-      console.log("initRight", initRight);
-      console.log("exceededPosRight", exceededPosRight);
       if (exceededPosRight) {
         return snapToPosition(this.target, `-${exceededPosRight}`);
       }
@@ -78,48 +67,30 @@ export const enableDrag = () => {
   const snapToPosition = (target: HTMLElement, x: number | string) => {
     gsap.to(target, {
       x,
-      duration: 0.3,
+      duration,
       ease: "power2.easeOut",
     });
   };
 
   const slowDown = (target: HTMLElement) => {
-    console.log("slowingdown");
-    const vx = velocity / (2 * friction);
+    const vx = velocity * duration - 0.5 * friction * Math.pow(duration, 2);
     const x = (() => {
       let position;
       if (direction === "left") {
-        console.log("vx", vx);
-        console.log("targetRect.left", targetRect.left);
-        console.log("endRect.left", endRect.left);
-        console.log("endRect.left - vx", endRect.left - vx);
-        console.log(
-          "endRect.width + targetRect.left - window.innerWidth",
-          endRect.width + targetRect.left - window.innerWidth
-        );
-        // willExceed = endRect.left - vx > endRect.width - targetRect.left;
         position =
-          endRect.left - vx >
-          endRect.width + targetRect.left - window.innerWidth
-            ? endRect.width - window.innerWidth
+          Math.abs(endRect.left - vx) >= endRect.width - window.innerWidth
+            ? `-${endRect.width - window.innerWidth}`
             : endRect.left - vx - targetRect.left;
-        console.log("position", position);
       }
       if (direction === "right") {
-        console.log("endRect.left", endRect.left);
-        console.log("vx", vx);
-        console.log("endRect.left + vx", endRect.left + vx);
-        console.log("targetRect.left", targetRect.left);
-        position = endRect.left + vx >= targetRect.left ? 0 : endRect.left + vx;
+        position = endRect.left + vx >= 0 ? 0 : endRect.left + vx;
       }
       return position;
     })();
-    const vt = (dist / velocity) * tt;
-    console.log("{vx, x, vt}", { x, vx, vt });
 
     gsap.to(target, {
       x,
-      duration: 0.3,
+      duration,
       ease: "power2.easeOut",
     });
 
